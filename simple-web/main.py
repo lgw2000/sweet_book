2f2d4518839b90675fb1be0f6c8c18ce93363cf3
lgw2000 <ace9907@naver.com>
2026-04-26 18:35:03 +0900

    Allow configurable database URL

--- simple-web/main.py
+++ simple-web/main.py
@@ -15,7 +15,7 @@ import shutil
 import string
 import feed_algorithm
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./users.db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./users.db")
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
